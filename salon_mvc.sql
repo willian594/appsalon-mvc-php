1b9099e93ba5e4cc949aae31b708a2c3043c79f6
willian Diaz <william44jf@gmail.com>
2025-11-11 20:49:27 -0600

    first commit

---
+++ salon_mvc.sql
@@ -0,0 +1,95 @@
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+DROP TABLE IF EXISTS `citas`;
+CREATE TABLE `citas` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `fecha` date DEFAULT NULL,
+  `hora` time DEFAULT NULL,
+  `usuarioId` int DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `usuarioId` (`usuarioId`),
+  CONSTRAINT `citas_ibfk_1` FOREIGN KEY (`usuarioId`) REFERENCES `usuarios` (`id`) ON DELETE SET NULL ON UPDATE SET NULL
+) ENGINE=InnoDB AUTO_INCREMENT=26 DEFAULT CHARSET=utf8mb3;
+
+DROP TABLE IF EXISTS `citasservicios`;
+CREATE TABLE `citasservicios` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `citaId` int DEFAULT NULL,
+  `servicioId` int DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `citaId` (`citaId`),
+  KEY `servicioId` (`servicioId`),
+  CONSTRAINT `citasservicios_ibfk_3` FOREIGN KEY (`citaId`) REFERENCES `citas` (`id`) ON DELETE SET NULL ON UPDATE SET NULL,
+  CONSTRAINT `citasservicios_ibfk_4` FOREIGN KEY (`servicioId`) REFERENCES `servicios` (`id`) ON DELETE SET NULL ON UPDATE SET NULL
+) ENGINE=InnoDB AUTO_INCREMENT=27 DEFAULT CHARSET=utf8mb3;
+
+DROP TABLE IF EXISTS `servicios`;
+CREATE TABLE `servicios` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `nombre` varchar(60) DEFAULT NULL,
+  `precio` decimal(5,2) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=13 DEFAULT CHARSET=utf8mb3;
+
+DROP TABLE IF EXISTS `usuarios`;
+CREATE TABLE `usuarios` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `nombre` varchar(60) DEFAULT NULL,
+  `apellido` varchar(60) DEFAULT NULL,
+  `email` varchar(30) DEFAULT NULL,
+  `password` varchar(60) DEFAULT NULL,
+  `telefono` varchar(10) DEFAULT NULL,
+  `admin` tinyint(1) DEFAULT NULL,
+  `confirmado` tinyint(1) DEFAULT NULL,
+  `token` varchar(15) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=13 DEFAULT CHARSET=utf8mb3;
+
+INSERT INTO `citas` (`id`, `fecha`, `hora`, `usuarioId`) VALUES
+(22, '2023-11-29', '10:30:00', 9),
+(23, '2025-10-14', '14:00:00', 12),
+(24, '2025-10-13', '12:05:00', 12),
+(25, '2025-10-21', '14:00:00', 12);
+INSERT INTO `citasservicios` (`id`, `citaId`, `servicioId`) VALUES
+(18, 22, 2),
+(19, 23, 2),
+(20, 23, 6),
+(21, 24, 2),
+(22, 24, 5),
+(23, 25, 1),
+(24, 25, 4),
+(25, 25, 7),
+(26, 25, 10);
+INSERT INTO `servicios` (`id`, `nombre`, `precio`) VALUES
+(1, 'Corte de Cabello Mujer', '70.00'),
+(2, 'Corte de Cabello Hombre', '80.00'),
+(3, 'Corte de Cabello Niño', '60.00'),
+(4, 'Peinado Mujer', '80.00'),
+(5, 'Peinado Hombre', '60.00'),
+(6, 'Peinado Niño', '60.00'),
+(7, 'Corte de Barba', '30.00'),
+(8, 'Tinte Mujer', '150.00'),
+(9, 'Uñas', '200.00'),
+(10, 'Lavado de Cabello', '50.00'),
+(11, 'Tratamiento Capilar', '50.00');
+INSERT INTO `usuarios` (`id`, `nombre`, `apellido`, `email`, `password`, `telefono`, `admin`, `confirmado`, `token`) VALUES
+(9, 'Junior', 'Administrador', 'correo@correo.com', '$2y$10$9TTiKdZXQaUQaSbVKd7wPOucLusU8ebkv2h2IgqNjQXs.uLTW7CAq', '1234567890', 1, 1, ''),
+(10, ' willian', 'Diaz', 'willian@gmail.com', '$2y$10$MZ5nPrLF.qxR0dW.ePnbpOoe8RlqPtEdBDcexLOMdD/iYdcp14iX2', '41116889', 1, 1, '68ddac81d1b36 '),
+(11, ' Samuel', 'Alexander', 'samuel@gmail.com', '$2y$10$/s4vxPJlQBjdsn0tV1yQMeqgv.m/kG9ZCRpRSyeULL/.kFSVB8ztS', '23452375', 0, 1, ''),
+(12, ' Alex', 'perez', 'alex@gmail.com', '$2y$10$9KwM4OBvalYeXh4ZWthRg.xnXL10BOf.YwP0bWT5/OeKOKOo.YQT.', '6123143', 0, 1, '');
+
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
--- views/admin/index.php
+++ views/admin/index.php
@@ -54,7 +54,7 @@
                 $proximo = $citas[$key + 1]->id ?? 0;
 
                 if(esUltimo($actual, $proximo)) { ?>
-                    <p class="total">Total: <span>$ <?php echo $total; ?></span></p>
+                    <p class="total">Total: <span>Q. <?php echo $total; ?></span></p>
 
                     <form action="/api/eliminar" method="POST">
                         <input type="hidden" name="id" value="<?php echo $cita->id; ?>">
